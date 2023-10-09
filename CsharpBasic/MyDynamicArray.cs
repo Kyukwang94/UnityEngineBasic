98c4bdfd1cbc7ff12bf8f7da8fabbbc196a42893
Kyukwang94 <dlrbrhkd94@naver.com>
2023-10-09 12:22:43 +0900

    Study

---
+++ CsharpBasic/MyDynamicArray.cs
@@ -0,0 +1,128 @@
+﻿// PascalCase : 사용자정의 자료형이름, 함수 이름, 프로퍼티 이름, public/protected 멤버 변수 이름
+// camelCase : 지역변수, (Unity API 활용할때는 public/protected 멤버 변수 이름도 이렇게 주로 씀)
+// _camelCase : private 멤버 변수 이름
+// snake_case : 상수 정의시
+// UPPER_SNAKE_CASE : 상수 정의시
+// m_Hungarian , iNum, fX <- 요즘날에는 잘안씀 : static 멤버변수 s_Instance
+
+namespace Collections
+{
+    internal class MyDynamicArray
+    {
+       
+        private const int DEFAULT_SIZE = 1;
+       
+        private object[] _items = new object[DEFAULT_SIZE]; // items[0] 부터 시작.
+        public object this[int index] //MyDynamicArray inventory = new MyDynamicArray(); inventory = this why ? MyDynamicArray value
+        {                           // inventory[0] format 지킬때만 불러준다.
+            get //  var get = inventory[0]
+            {
+                if (index < 0 || index >= _count) //해당 방번호가 0보다 작은곳이거나 해당 방번호가 실질거주민의 주소보다 크거나 같으면.
+                {
+                    throw new IndexOutOfRangeException(); 
+                }
+
+                return _items[index]; // _items[index]의 주소로 접근해 안에 값을 주겠다.
+            }
+            set // inventroy[0] = new slotData((int)redPotion , 60)
+            {
+                if (index < 0 || index >= _count)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+
+                _items[index] = value; //value는 main에서 선언된값이들어가는거
+            }
+        }
+        private int _count;
+        public int Count //_count 접근하고싶으면 Count를 통해서 불러라.
+        {
+            get
+            {
+                return _count;
+            }
+        }
+        public int Capacity
+        {
+          get
+            {
+                return _items.Length;
+            }
+        }
+
+       /* public int Count => _count;
+        public int Capacity => _items.Length;*/
+
+       
+       
+
+        public void Add(object item) // 현재꽉차있는 가방에 Add함수를 다시한번더 쓰면 가방칸을 늘려주거나 item을 추가해줌.
+        {
+
+           
+            if (_count >= _items.Length)
+            {
+                object[] tmp = new object[_count * 2]; 
+                Array.Copy(_items, tmp, _count); //기존배열 새로운배열에다가 어디까지 _count까지.
+                _items = tmp; //tmp만큼 * 2 된 배열만큼 늘어남 하지만 값은 없음.
+            }
+
+            _items[_count++] = item;//새로운 아이템을 넣는다
+
+        }
+
+        public object Find(Predicate<object> match) 
+        {
+            for (int i = 0; i < _count; i++)
+            {
+                if (match(_items[i]))
+                    return _items[i];
+            }
+            return default;
+        }
+       
+        public int FindIndex(Predicate<object> match)  // 그저형식만체크
+        {                                             
+            for (int i = 0; i < _count; i++)         //count만큼 i를 올림.
+            {
+                if (match(_items[i]))  //items[i]를 lambda변수 SlotData에 넘겨줌. match는 해당 파라미터로 인해 lambda조건체크.
+                    return i;          //해당 i에서 match되면 return  i (지금 index)
+            }
+            return -1;
+        }
+
+        public bool Contains(object item)//해당 아이템이잇는지 확인
+        {
+            for (int i = 0; i < _count; i++)
+            {
+                if (_items[i] == item)
+                    return true;
+            }
+
+            return false;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index >= _count) //해당인덱스가 범위인덱스에서 벗어났을때
+                throw new IndexOutOfRangeException();
+
+            for (int i = index; i < _count - 1; i++)
+            {
+                _items[i] = _items[i + 1];
+            }
+            _count--;
+        }
+
+        public bool Remove(object item)
+        {
+            int index = FindIndex(x => x == item);
+
+            if (index < 0)
+                return false;
+
+            RemoveAt(index);
+            return true;
+        }
+    }
+}
---
+++ CsharpBasic/Program.cs
@@ -0,0 +1,113 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+
+namespace Collections
+{
+    enum ItemID
+    {   
+        Arrow  = 10,
+        RedPotion = 20,
+        BluePotion = 21,
+    }
+
+    class SlotData : IComparable<SlotData>
+    {
+        public bool isEmpty => id == 0 && num == 0;
+
+        public int id;
+        public int num;
+
+        public SlotData(int id, int num)
+        {
+            this.id = id;
+            this.num = num;
+        }
+
+        public int CompareTo(SlotData? other)
+        {
+            return this.id == other?.id && this.num == other.num ? 0 : -1;  // 다른 것과 비교해서 같으면 정렬할수있게하는 기능으로 만들수있음.
+        }                                                                    //아니면 비교해서같으면 해당개수를 합치게하는것도가능할것같음
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDynamicArray inventory = new MyDynamicArray();
+
+            for (int i = 0; i < 4; i++)
+            {
+                inventory.Add(new SlotData(0, 0));
+            }
+            inventory.Add(new SlotData(0, 0));
+            inventory[3] = new SlotData((int)ItemID.BluePotion, 30);
+           
+            // todo -> 파란포션 5개를 획득
+            // 1. 파란포션 5개가 들어갈 수 있는 슬롯을 찾아야함.   // obejct가 들어가고 bool값으로 나올텐데 match가 call되면 안의값을 비교하게됨 형식을 넘기고 기능은 나중에
+            int availableSlotIndex = inventory.FindIndex(IsSlotData => ((SlotData)IsSlotData).isEmpty || // unboxing
+                                                                       (((SlotData)IsSlotData).id == (int)ItemID.BluePotion &&
+                                                                       ((SlotData)IsSlotData).num <= 99 - 20));
+            object finditem = inventory.Find(FindItem => ((SlotData)FindItem).id == (int)ItemID.BluePotion);
+                ItemID itemId = (ItemID)((SlotData)finditem).id;
+            int itemNumber = (int)(ItemID)((SlotData)finditem).num;
+            int itemIndex = (int)(ItemID)((SlotData)finditem).id;
+                Console.WriteLine($"Found Item ID:" +
+                    $"{itemId}, {itemIndex},{itemNumber}");
+            
+            /*  bool slotData(생략) (IsSlotData)    // 
+             {
+               return ((SlotData)IsSlotData).isEmpty ||  
+                      (((SlotData)IsSlotData).id == (int)ItemID.BluePotion &&
+                      ((SlotData)IsSlotData).num <= 99 - 20));  
+                     slotData is Empty OR slotData.id == int BluePotion AND slotData.num <= 79 reutrn true or false;
+              } */
+
+            int availableSlotIndex2 = inventory.FindIndex(slotData2 => ((SlotData)slotData2).isEmpty ||
+                                                                     (((SlotData)slotData2).id == (int)ItemID.RedPotion &&
+                                                                      ((SlotData)slotData2).num <= 99 - 30));
+                                              //FindIndex한테 value를 넘겨줄건데 만약 slotData라는 변수를 넣어줄테니까 안에 계산식들이 모두 통과되었을때 넘겨준다.
+
+            // 2. 해당 슬롯의 아이템 갯수에다가 내가 추가하려는 갯수를 더한 만큼의 수정예상값을 구함.
+            int expected = ((SlotData)inventory[availableSlotIndex]).num + 20;
+            int expected2 = ((SlotData)inventory[availableSlotIndex2]).num + 30;
+
+            // 3. 새로운 아이템 데이터를 만들어서 슬롯 데이터를 교체 해줌.
+            SlotData newSlotData = new SlotData((int)ItemID.BluePotion, expected);
+            SlotData newSlotData2 = new SlotData((int)ItemID.RedPotion, expected2);
+            inventory[availableSlotIndex] = newSlotData;
+            inventory[availableSlotIndex2] = newSlotData2;
+
+            Console.WriteLine("인벤토리 정보 :");
+           
+            Console.WriteLine($"Capacity : {inventory.Capacity} , Counts : {inventory.Count}");
+
+            object FindItemRemove = inventory.Find(slotdata => ((SlotData)slotdata).id == (int)ItemID.BluePotion); // 해당인덱스에있는 아이템이사라짐
+            bool removed = inventory.Remove(FindItemRemove);
+            Console.WriteLine(removed); 
+           /* inventory.RemoveAt(3); // index 자체가 사라짐*/
+
+            for (int i = 0; i < inventory.Count; i++)
+            {
+                Console.WriteLine($"슬롯 {i} : [{(ItemID)((SlotData)inventory[i]).id}] , [{((SlotData)inventory[i]).num}]");
+            }
+            MyDynamicArray<SlotData> inventory2 = new MyDynamicArray<SlotData>();
+
+            // 숙제 : MyDynamicArray 기반으로 만들었던 예제를 inventory2 가지고 구현해보기
+
+            /*
+            inventory[1] = "철수";
+            Console.WriteLine($"동적배열의 0번째 인덱스 값 : {inventory[0]}");
+
+            int idx = inventory.FindIndex(x => (string)x == "철수");
+            idx = inventory.FindIndex(x => (int)x > 3);
+
+            int 파란포션5개추가가능한인덱스 = inventory.FindIndex(x => x.isEmpty == true ||
+                                                                (x.id == 파란포션아이디 &&
+                                                                 x.num <= 파란포션최대갯수 - 5))
+            int 수정될갯수 = inventory[파란포션5개추가가능한인덱스].num + 5;
+            슬롯데이터 수정될슬롯데이터 = new 슬롯데이터(파란포션아이디, 수정될갯수);
+            inventory[파란포션5개추가가능한인덱스] = 수정될슬롯데이터;
+            */
+        }
+    }
+}
